1539da0732c24d615ccd160c9e18fb79aba858b0
c2chance <chance.chih@gmail.com>
2025-11-20 21:46:42 +0800

    add get("/") function

---
+++ java_evthset/accessingdatasqlite3/src/main/java/com/ideen/accessingdatasqlite3/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.ideen.accessingdatasqlite3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        // Forward to the static index.html under src/main/resources/static
+        return "forward:/index.html";
+    }
+
+}
