39fb56d6326112138a90faffbf62ebce14f77e76
c2chance <1990542+c2chance@users.noreply.github.com>
2025-11-21 05:04:28 +0800

    Add new accessingdatamongodb

---
+++ java_evthset/accessingdatamongodb/src/main/java/com/ideen/accessingdatamongodb/AccessingdatamongodbApplication.java
@@ -0,0 +1,50 @@
+package com.ideen.accessingdatamongodb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingdatamongodbApplication implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingdatamongodbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		repository.deleteAll();
+
+		// save a few objects
+		repository.save(new Customer("Alice", "Smith"));
+		repository.save(new Customer("Bob", "Smith"));
+		repository.save(new Customer("Charlie", "Brown"));
+		repository.save(new Customer("David", "Johnson"));
+		repository.save(new Customer("Eve", "Davis"));
+
+		// fetch all customers
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.printLn(repository.findByFirstName("Alice"));
+		System.out.println();
+
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.printLn(customer);
+		}
+		System.out.println();
+	}
+}
---
+++ java_evthset/accessingdatamongodb/src/test/java/com/ideen/accessingdatamongodb/AccessingdatamongodbApplicationTests.java
@@ -0,0 +1,77 @@
+package com.ideen.accessingdatamongodb;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.data.domain.Example;
+
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+@SpringBootTest
+@Testcontainers
+class AccessingdatamongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Container
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.2");
+
+	@Autowired
+	private CustomerRepository repository;
+	
+	Customer dave, oliver, carter;
+
+	@BeforeEach
+	public void setUp() {
+
+		repository.deleteAll();
+
+		dave = new Customer("Dave", "Matthews");
+		oliver = new Customer("Oliver", "Matthews");
+		carter = new Customer("Carter", "Beauford");
+				
+		repository.save(dave);
+		repository.save(oliver);
+		repository.save(carter);
+	}
+
+	@Test
+	public void setsIdonSave() {
+		Customer john = new Customer("John", "Doe");
+
+		assertThat(john.id).isNotNull();
+	}
+
+	@Test
+	public void findsByLastName() {
+		List<Customer> customers = repository.findByLastName("Matthews");
+
+		assertThat(customers).hasSize(2).extracting(Customer::getFirstName).containsOnly("Dave", "Oliver");
+	}
+
+	@Test
+	public void findsByExample() {
+		Customer probe = new Customer();
+		probe.setLastName("Matthews");
+
+		// Example<Customer> example = Example.of(probe);
+
+		List<Customer> customers = repository.findAll(probe);
+
+		assertThat(customers).hasSize(2).extracting(Customer::getFirstName).containsOnly("Dave", "Oliver");
+	}
+
+}
