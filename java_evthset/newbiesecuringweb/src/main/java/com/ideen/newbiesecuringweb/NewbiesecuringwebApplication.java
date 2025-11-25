ff4e662ce7a9864f40883e918e0ac2d837cf2982
c2chance <1990542+c2chance@users.noreply.github.com>
2025-11-26 05:31:26 +0800

    add new securingweb project

---
+++ java_evthset/newbiesecuringweb/src/main/java/com/ideen/newbiesecuringweb/NewbiesecuringwebApplication.java
@@ -0,0 +1,13 @@
+package com.ideen.newbiesecuringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbiesecuringwebApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(NewbiesecuringwebApplication.class, args);
+	}
+
+}
