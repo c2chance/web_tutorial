0de446ada8643e655f069117f4f64af35dbc4dac
c2chance <1990542+c2chance@users.noreply.github.com>
2025-12-02 05:56:09 +0800

    init a new project from springio, gstestingweb

---
+++ java_evthset/gstestingweb/src/main/java/com/ideen/gstestingweb/GstestingwebApplication.java
@@ -0,0 +1,13 @@
+package com.ideen.gstestingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstestingwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstestingwebApplication.class, args);
+	}
+
+}
