2bf69340eb992e70fb1b6027e66ad3b956a5baca
c2chance <1990542+c2chance@users.noreply.github.com>
2025-12-04 03:59:05 +0800

    init a project how to use https, cannot work. need time to solve it

---
+++ java_evthset/usehttpscase/src/test/java/com/ideen/usehttpscase/UsehttpscaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideen.usehttpscase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsehttpscaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
