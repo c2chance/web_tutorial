f0121d1c8666d97225388f776439717ec7eadf26
c2chance <1990542+c2chance@users.noreply.github.com>
2025-12-04 05:06:40 +0800

    init new project

---
+++ java_evthset/restfulwebservicewithcors/src/test/java/com/ideen/restservicecors/RestfulwebservicewithcorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideen.restservicecors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwebservicewithcorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
